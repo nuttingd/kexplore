8741bc7e9d033e2aa30444a3e86a1b6bfe64a78f
David Nutting <david@nutting.dev>
2026-02-01 19:20:17 -0800

    chore: remove duplicate and unused imports in EventStreamScreen

--- app/src/main/java/dev/nutting/kexplore/ui/screen/events/EventStreamScreen.kt
+++ app/src/main/java/dev/nutting/kexplore/ui/screen/events/EventStreamScreen.kt
@@ -1,6 +1,5 @@
 package dev.nutting.kexplore.ui.screen.events
 
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -8,7 +7,6 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.rememberLazyListState
@@ -30,7 +28,6 @@ import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
